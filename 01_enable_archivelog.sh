1e03e2dd52a4bbd5c2452d1b2fef53ad8d51987f
Patrick <patricktsaikl@gmail.com>
2025-07-07 16:15:13 +0800

    fix/add supplemental loog

--- 01_enable_archivelog.sh
+++ 01_enable_archivelog.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# This script enables ARCHIVELOG mode as SYS
+# This script enables ARCHIVELOG mode and supplemental logging as SYS
 
 sqlplus / as sysdba <<-EOF
-  -- Abort any sessions that might prevent a clean shutdown
-  ALTER SYSTEM KILL SESSION 'sid,serial#' IMMEDIATE;
   SHUTDOWN IMMEDIATE;
   STARTUP MOUNT;
   ALTER DATABASE ARCHIVELOG;
   ALTER DATABASE OPEN;
   -- Ensure the PDB is also open and ready
   ALTER PLUGGABLE DATABASE FREEPDB1 OPEN;
+  -- Use the general supplemental logging command that is proven to work
+  ALTER DATABASE ADD SUPPLEMENTAL LOG DATA;
+  -- somehow ALTER DATABASE ADD SUPPLEMENTAL LOG DATA (PRIMARY KEY) COLUMNS; not working
   EXIT;
 EOF
